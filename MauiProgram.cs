003ea1c4851a48bf4b7dd0bc7bc04684e69c1458
duydang2412 <duydang2412@gmail.com>
2022-05-08 23:07:26 +0700

    feat(Resources): add images, fonts

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -13,6 +13,9 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				fonts.AddFont("Inter-Regular.ttf", "InterRegular");
+				fonts.AddFont("Inter-Bold.ttf", "InterBold");
+				fonts.AddFont("Inter-Light.ttf", "InterLight");
 			})
 			.UseMauiCommunityToolkit();
 
