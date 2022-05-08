e880c80eee71aa787f231845412887247fc1a4a9
duydang2412 <duydang2412@gmail.com>
2022-05-08 17:29:57 +0700

    feat(DrawingView): handle StartInteraction

--- DrawingViews/DrawingView.xaml.cs
+++ DrawingViews/DrawingView.xaml.cs
@@ -1,7 +1,10 @@
+using Maporizer.DrawingViews.Models;
+
 namespace Maporizer.DrawingViews;
 
 public partial class DrawingView : ContentView
 {
+	private const int defaultSize = 100;
 	public DrawingView()
 	{
 		InitializeComponent();
@@ -12,4 +15,16 @@ public partial class DrawingView : ContentView
 		var touch = e.Touches[0];
 		CoordinateLabel.Text = $"X: {touch.X}  Y: {touch.Y}";
 	}
+
+    private void GraphicsView_StartInteraction(object sender, TouchEventArgs e)
+    {
+		(GraphicsView.Drawable as GraphicsDrawable).Draw(new EllipseModel
+		{
+			Location = e.Touches[0].Offset(-defaultSize / 2, -defaultSize / 2),
+			Size = new Size(defaultSize),
+			FillColor = App.Current.Resources["Primary"] as Color,
+			StrokeColor = App.Current.Resources["Secondary"] as Color
+        });
+		GraphicsView.Invalidate();
+    }
 }
