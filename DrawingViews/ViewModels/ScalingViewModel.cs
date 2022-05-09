3aec63d72a473c054d1e6fbfe760bc85a07c09c2
duydang2412 <duydang2412@gmail.com>
2022-05-09 23:25:48 +0700

    feat(ScalingViewModel): update GraphicsView size on scaling

--- DrawingViews/ViewModels/ScalingViewModel.cs
+++ DrawingViews/ViewModels/ScalingViewModel.cs
@@ -8,10 +8,14 @@ public partial class DrawingView : ContentView
 
     private void Slider_ValueChanged(object? sender, ValueChangedEventArgs e)
     {
+        var drawable = (GraphicsDrawableModel)GraphicsView.Drawable;
         int remainder = (int)Math.Round(e.NewValue / scaleUnit);
         var snappedValue = remainder * 25;
+        var oldScaleFactor = drawable.ScaleFactor;
         (sender as Slider)!.Value = snappedValue;
-        ((GraphicsDrawableModel)GraphicsView.Drawable).ScaleFactor = snappedValue / 100f;
+        GraphicsView.WidthRequest = GraphicsView.WidthRequest / oldScaleFactor * (snappedValue / 100f);
+        GraphicsView.HeightRequest = GraphicsView.HeightRequest / oldScaleFactor * (snappedValue / 100f);
+        drawable.ScaleFactor = snappedValue / 100f;
         GraphicsView.Invalidate();
     }
 }
