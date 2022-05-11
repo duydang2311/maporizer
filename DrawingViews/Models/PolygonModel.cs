40cc9698d4b9c1670a5b8112ad2cbcc68d313aef
duydang2412 <duydang2412@gmail.com>
2022-05-12 00:33:53 +0700

    feat(DrawingViews): implement Polygon drawing model

---
+++ DrawingViews/Models/PolygonModel.cs
@@ -0,0 +1,39 @@
+﻿namespace Maporizer.DrawingViews.Models;
+
+public class PolygonModel : DrawingBaseModel
+{
+    private readonly PathF _path;
+    public float StrokeWidth { get; set; }
+    public PolygonModel() : base()
+    {
+        _path = new PathF();
+        StrokeWidth = 5f;
+    }
+    public void Close()
+    {
+        _path.Close();
+    }
+    public void Open()
+    {
+        _path.Open();
+    }
+    public void Add(PointF point)
+    {
+        // TODO: quad or curve if the path formed up a polygon
+        _path.LineTo(point);
+    }
+    public override void Draw(ICanvas canvas, RectF dirtyRect)
+    {
+        canvas.StrokeSize = StrokeWidth;
+        canvas.StrokeColor = StrokeColor;
+        canvas.FillColor = FillColor;
+        canvas.FillPath(_path, WindingMode.EvenOdd);
+        canvas.DrawPath(_path);
+        
+    }
+    public override bool IsCollidedWith(PointF point)
+    {
+        // TODO: implement
+        return false;
+    }
+}
