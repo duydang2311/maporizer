bce0268bc417b4505d5b7fbc93330b4c7dbbfdae
duydang2412 <duydang2412@gmail.com>
2022-05-14 23:00:02 +0700

    feat(DrawingBatch): make interaction thread-safe

--- DrawingViews/ViewModels/DrawingBatch/DrawingBatchViewModel.Interaction.cs
+++ DrawingViews/ViewModels/DrawingBatch/DrawingBatchViewModel.Interaction.cs
@@ -6,29 +6,57 @@ namespace Maporizer.DrawingViews.ViewModels.DrawingBatch;
 public partial class DrawingBatchViewModel
 {
     private bool interacting;
+    private bool disposed;
     private PointF lastPoint;
+    private CancellationTokenSource tokenSource;
+    private object mutex = new ();
     public void InitInteractionInternal()
     {
         interacting = false;
+        disposed = true;
         View.StartInteraction += View_StartInteraction;
         View.MoveHoverInteraction += View_MoveHoverInteraction;
         View.EndInteraction += View_EndInteraction;
     }
+    private int i = 0;
     private void View_StartInteraction(object? sender, TouchEventArgs e)
     {
-        if (PolygonBatch is null)
+        if (disposed)
         {
             interacting = true;
-            Task.Run(() =>
+            lock (mutex)
             {
-                Thread.Sleep(50);
-                if (!interacting)
+                if (!disposed && tokenSource is not null)
+                {
+                    tokenSource.Cancel();
+                    tokenSource.Dispose();
+                }
+                tokenSource = new CancellationTokenSource();
+                disposed = false;
+            }
+            var waitHandle = tokenSource.Token.WaitHandle;
+            var task = Task.Run(() =>
+            {
+                bool cancelled = false;
+                lock (mutex)
+                {
+                    cancelled = waitHandle.WaitOne(100);
+                    tokenSource.Dispose();
+                    disposed = true;
+                }
+                if (cancelled || !interacting)
                 {
                     return;
                 }
-                PolygonBatch = new PolygonModel { StrokeColor = Colors.White };
-                PolygonBatch.Scale(((GraphicsDrawableModel)View.Drawable).ScaleFactor);
-                ((GraphicsDrawableModel)View.Drawable).Draw(PolygonBatch);
+                App.Current.Dispatcher.Dispatch(() =>
+                {
+                    if (interacting)
+                    {
+                        PolygonBatch = new PolygonModel { StrokeColor = Colors.White };
+                        PolygonBatch.Scale(((GraphicsDrawableModel)View.Drawable).ScaleFactor);
+                        ((GraphicsDrawableModel)View.Drawable).Draw(PolygonBatch);
+                    }
+                });
             });
         }
     }
@@ -44,6 +72,13 @@ public partial class DrawingBatchViewModel
                 PolygonBatch = null;
                 lastPoint = Point.Zero;
                 View.Invalidate();
+            }
+            lock (mutex)
+            {
+                if (!disposed)
+                {
+                    tokenSource.Cancel();
+                }
             }
         }
     }
