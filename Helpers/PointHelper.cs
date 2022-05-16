9c235697e57fa8e6d9f562ca96ea35c10e5c7a01
duydang2412 <duydang2412@gmail.com>
2022-05-16 16:04:02 +0700

    feat(PointHelper): add DistanceToLineSquared

--- Helpers/PointHelper.cs
+++ Helpers/PointHelper.cs
@@ -14,4 +14,28 @@ public static class PointHelper
         float dy = a.Y - b.Y;
         return dx * dx + dy * dy;
     }
+    public static float DistanceToLineSquared(PointF point, PointF lineStart, PointF lineEnd)
+    {
+        var lengthSquared = (float)DistanceSquared(lineStart, lineEnd);
+        if (lengthSquared == 0)
+            return (float)DistanceSquared(point, lineStart);
+        var t = ((point.X - lineStart.X) * (lineEnd.X - lineStart.X) + (point.Y - lineStart.Y) * (lineEnd.Y - lineStart.Y)) / lengthSquared;
+        if (t < 0)
+        {
+            return (float)DistanceSquared(point, lineStart);
+        }
+        else if (t > 1.0)
+        {
+            return (float)DistanceSquared(point, lineEnd);
+        }
+        else
+        {
+            var proj = new PointF
+            (
+                lineStart.X + t * (lineEnd.X - lineStart.X),
+                lineStart.Y + t * (lineEnd.Y - lineStart.Y)
+            );
+            return (float)DistanceSquared(point, proj);
+        }
+    }
 }
