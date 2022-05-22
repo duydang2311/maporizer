f524ec2634d0fd8ea4df23c1b0d1dfde85decafc
duydang2412 <duydang2412@gmail.com>
2022-05-22 23:29:37 +0700

    feat(Helpers): add ThemeHelper

---
+++ Helpers/ThemeHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace Maporizer.Helpers;
+
+public static class ThemeHelper
+{
+    public static ImageSource GetImageSource(string path)
+    {
+        return ImageSource.FromFile
+        (
+            App.Current!.PlatformAppTheme != AppTheme.Dark
+            ? path + "_light.png"
+            : path + "_dark.png"
+        );
+    }
+}
