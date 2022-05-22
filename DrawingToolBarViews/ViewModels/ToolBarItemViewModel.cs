2e4c07f45ad6e2220bd26aac41e7c30b16080af4
duydang2412 <duydang2412@gmail.com>
2022-05-22 23:31:28 +0700

    feat(ToolBar): add ToolBarItemViewModel

---
+++ DrawingToolBarViews/ViewModels/ToolBarItemViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+
+namespace Maporizer.DrawingToolBarViews.ViewModels;
+
+public class ToolBarItemViewModel : INotifyPropertyChanged
+{
+    private ImageSource source;
+    public Command Command { get; }
+
+    public ImageSource Source
+    {
+        get => source;
+        set
+        {
+            if (source != value)
+            {
+                source = value;
+                if (PropertyChanged is not null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(nameof(Source)));
+                }
+            }
+        }
+    }
+    public event PropertyChangedEventHandler? PropertyChanged;
+    public ToolBarItemViewModel(ImageSource source, Action execute)
+    {
+        this.source = source;
+        Command = new Command(execute);
+    }
+}
