7c9e11b8f1cbdf75f930a7e9c6daefc1f662ae14
duydang2412 <duydang2412@gmail.com>
2022-05-22 23:08:54 +0700

    refactor(ToolBar)!: delete ToolBarItemModel

--- DrawingToolBarViews/Models/ToolBarItemModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Maporizer.DrawingToolBarViews.Models;
-
-public class ToolBarItemModel
-{
-    public ImageSource Source { get; set; } = null!;
-    public Command Command { get; set; } = null!;
-}
