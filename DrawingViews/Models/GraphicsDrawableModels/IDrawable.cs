4008b905164edf81a36a70ffb8557c6bc74fd66f
duydang2412 <duydang2412@gmail.com>
2022-05-24 23:19:02 +0700

    feat(IDrawable): add HoveringDrawing member

--- DrawingViews/Models/GraphicsDrawableModels/IDrawable.cs
+++ DrawingViews/Models/GraphicsDrawableModels/IDrawable.cs
@@ -4,6 +4,7 @@ public interface IDrawable : Microsoft.Maui.Graphics.IDrawable
 {
     LinkedList<IDrawableShape> Drawings { get; }
     GraphicsView GraphicsView { get; }
+    IDrawableShape? HoveringDrawing { get; }
     void Erase(IDrawableShape drawing);
     void Draw(IDrawableShape drawing);
 }
