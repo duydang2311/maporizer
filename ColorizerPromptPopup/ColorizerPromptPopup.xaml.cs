f2ad30ddeb6f6b711d3c1476d808b51486664e1e
duydang2412 <duydang2412@gmail.com>
2022-05-26 22:47:22 +0700

    feat(ColorizerPromptPopup): add ColorizerPromptPopup

---
+++ ColorizerPromptPopup/ColorizerPromptPopup.xaml.cs
@@ -0,0 +1,11 @@
+using CommunityToolkit.Maui.Views;
+
+namespace Maporizer.ColorizerPopupViews;
+
+public partial class ColorizerPromptPopup : Popup
+{
+	public ColorizerPromptPopup()
+	{
+		InitializeComponent();
+	}
+}
