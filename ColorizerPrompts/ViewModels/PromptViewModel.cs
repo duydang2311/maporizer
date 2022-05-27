f4d39ee07804a3ab007a63548803d32493029bde
duydang2412 <duydang2412@gmail.com>
2022-05-27 21:23:12 +0700

    feat(PromptViewModel): bind entry, algorithm - handle OKCanExecute

--- ColorizerPrompts/ViewModels/PromptViewModel.cs
+++ ColorizerPrompts/ViewModels/PromptViewModel.cs
@@ -4,14 +4,42 @@ public class PromptViewModel
 {
     public Command OKCommand { get; }
     public Command CancelCommand { get; }
+    public string picked;
+    public string entry;
+    public string Picked
+    {
+        get => picked;
+        set
+        {
+            if (picked != value)
+            {
+                picked = value;
+                OKCommand.ChangeCanExecute();
+            }
+        }
+    }
+    public string Entry 
+    {
+        get => entry;
+        set
+        {
+            if (entry != value)
+            {
+                entry = value;
+                OKCommand.ChangeCanExecute();
+            }
+        }
+    }
     public PromptViewModel()
     {
+        picked = " ";
+        entry = " ";
         OKCommand = new Command(OKCommandHandler, OKCanExecuteHandler);
         CancelCommand = new Command(CancelCommandHandler);
     }
     private void OKCommandHandler()
     {
-        MessagingCenter.Send(this, "OK");
+        MessagingCenter.Send(this, "OK", new Models.PromptResultModel(Picked, byte.Parse(Entry)));
     }
     private void CancelCommandHandler()
     {
@@ -19,6 +47,6 @@ public class PromptViewModel
     }
     private bool OKCanExecuteHandler()
     {
-        return true;
+        return Picked.Length != 0 && byte.TryParse(Entry, out byte _);
     }
 }
