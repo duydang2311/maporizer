72a6811660824d47011dc2815cbba211e3c507df
duydang2412 <duydang2412@gmail.com>
2022-05-27 21:06:39 +0700

    feat(ColorizerPrompts): add PromptResultModel

---
+++ ColorizerPrompts/Models/PromptResultModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Maporizer.ColorizerPrompts.Models;
+
+public class PromptResultModel
+{
+    public string Algorithm { get; }
+    public int Colors { get; }
+    public PromptResultModel(string algorithm, int colors)
+    {
+        Algorithm = algorithm;
+        Colors = colors;
+    }
+}
