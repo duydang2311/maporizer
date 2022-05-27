7c2e2c663b84aa2371845a840fde6f29deba6a69
duydang2412 <duydang2412@gmail.com>
2022-05-28 00:57:50 +0700

    feat(MauiProgram): iinject IFileSavePicker

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,9 +1,13 @@
 ﻿using CommunityToolkit.Maui;
+using Maporizer.FileSavePickers;
+using Maporizer.MainPages.ViewModels.MenuBar;
+using Maporizer.MainPages;
 
 namespace Maporizer;
 
 public static class MauiProgram
 {
+	public static ServiceProvider ServiceProvider { get; private set; } = null!;
 	public static MauiApp CreateMauiApp()
 	{
 		var builder = MauiApp.CreateBuilder();
@@ -18,7 +22,12 @@ public static class MauiProgram
 				fonts.AddFont("Inter-Light.ttf", "InterLight");
 			})
 			.UseMauiCommunityToolkit();
-
-		return builder.Build();
+#if WINDOWS
+        builder.Services.AddTransient<IFileSavePicker, Platforms.Windows.FileSavePicker>();
+#elif MACCATALYST
+		builder.Services.AddTransient<IFileSavePicker, Platforms.MacCatalyst.FileSavePicker>();
+#endif
+		ServiceProvider = builder.Services.BuildServiceProvider();
+        return builder.Build();
 	}
 }
