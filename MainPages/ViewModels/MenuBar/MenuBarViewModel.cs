fa94b2486a13560c6032bc4ffdb6725b27c65867
duydang2412 <duydang2412@gmail.com>
2022-05-29 00:08:14 +0700

    refactor(MenuBarViewModel): remove DI pattern

--- MainPages/ViewModels/MenuBar/MenuBarViewModel.cs
+++ MainPages/ViewModels/MenuBar/MenuBarViewModel.cs
@@ -4,17 +4,8 @@ namespace Maporizer.MainPages.ViewModels.MenuBar;
 
 public partial class MenuBarViewModel
 {
-    private readonly IFileSavePicker fileSavePicker;
     public MenuBarViewModel()
     {
-        fileSavePicker = null!;
-        InitNewInternal();
-        InitImportInternal();
-        InitExportInternal();
-    }
-    public MenuBarViewModel(IFileSavePicker fileSavePicker)
-    {
-        this.fileSavePicker = fileSavePicker;
         InitNewInternal();
         InitImportInternal();
         InitExportInternal();
