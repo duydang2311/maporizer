929185d2594f148d1f2bdfc9617478d4a61db21d
duydang2412 <duydang2412@gmail.com>
2022-05-29 00:59:00 +0700

    feat(MainPage): alert when exported

--- MainPages/MainPage.xaml.cs
+++ MainPages/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Maporizer.MainPages.ViewModels.Colorizer;
 using Maporizer.MainPages.ViewModels.MenuBar;
+using Maporizer.DrawingViews;
 
 namespace Maporizer.MainPages;
 
@@ -10,10 +11,15 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 		_ = new ColorizerPromptViewModel(this);
 		MessagingCenter.Subscribe<MenuBarViewModel, string>(this, "Export", OnExport);
+		MessagingCenter.Subscribe<DrawingView>(this, "Exported", OnExported);
 	}
 	private void OnExport(MenuBarViewModel sender, string path)
     {
 		MessagingCenter.Send(this, "Export", path);
+    }
+	private async void OnExported(DrawingView sender)
+    {
+		await DisplayAlert("Export", "Your map has been exported successfully", "OK");
     }
 }
 
