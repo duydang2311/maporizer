4ff9da56248c493cf13e94e2ac585d9430f9394a
duydang2412 <duydang2412@gmail.com>
2022-05-30 13:23:21 +0700

    feat(ColorizeViewModel): stop colorizing if exit colorize mode

--- DrawingViews/ViewModels/ColorizeViewModel.cs
+++ DrawingViews/ViewModels/ColorizeViewModel.cs
@@ -3,6 +3,8 @@ using Maporizer.Helpers;
 using Maporizer.ColorizerPrompts.Models;
 using Maporizer.Colorizers;
 using Maporizer.DrawingViews.Models.GraphicsDrawableModels;
+using Maporizer.DrawingToolBarViews.ViewModels;
+using Maporizer.DrawingToolBarViews.Models;
 
 namespace Maporizer.DrawingViews.ViewModels;
 
@@ -10,11 +12,20 @@ public class ColorizeViewModel
 {
     private readonly IGraphicsDrawable drawable;
     private readonly DrawingView view;
+    private IColorizer? colorizer;
     public ColorizeViewModel(DrawingView view)
     {
         this.view = view;
         drawable = (IGraphicsDrawable)view.GraphicsView.Drawable;
         MessagingCenter.Subscribe<MainPage, PromptResultModel>(view, "Colorize", OnColorize);
+        MessagingCenter.Subscribe<DrawingToolBarViewModel, ToolBarItemViewModel>(this, "ItemSelected", OnItemSelected);
+    }
+    private void OnItemSelected(DrawingToolBarViewModel sender, ToolBarItemViewModel item)
+    {
+        if (colorizer is not null && item.Mode != DrawingMode.Colorize)
+        {
+            colorizer.Stop();
+        }
     }
     private void OnColorize(MainPage sender, PromptResultModel model)
     {
@@ -22,9 +33,8 @@ public class ColorizeViewModel
         {
             Task.Run(() =>
             {
-                var matrix = DrawableHelper.MakeAdjacencyMatrix(drawable);
-                var colorizer = new BruteForceColorizer(OnIteration, 100);
-                var result = colorizer.Colorize(matrix, model.Colors);
+                colorizer = new BruteForceColorizer(OnIteration, model.Delay);
+                var result = colorizer.Colorize(DrawableHelper.MakeAdjacencyMatrix(drawable));
             });
         }
     }
@@ -33,7 +43,6 @@ public class ColorizeViewModel
         var idx = 0;
         var colors = new Color[]
         {
-            Colors.Transparent,
             Colors.Green,
             Colors.Yellow,
             Colors.Red,
@@ -50,7 +59,8 @@ public class ColorizeViewModel
         {
             foreach (var i in drawable.Drawings)
             {
-                i.FillColor = colors[result[idx++] + 1];
+                i.FillColor = result[idx] == -1 ? Colors.Transparent : colors[result[idx]];
+                ++idx;
             }
             view.GraphicsView.Invalidate();
         });
